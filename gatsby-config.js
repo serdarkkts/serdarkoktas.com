ed28a7f64102e774731cfc3fd5cd26586cfba852
Serdar KÖKTAŞ <serdarkkts@gmail.com>
2020-03-22 00:36:43 +0300

    analytics config

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,7 +40,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+        trackingId: `UA-161551341-1`,
       },
     },
     `gatsby-plugin-sitemap`,
