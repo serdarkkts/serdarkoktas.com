4275875f58b702b7f8c773827e61671e416dfe58
Serdar KÖKTAŞ <serdarkkts@gmail.com>
2020-03-21 16:24:34 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+<p align="center">
+  <a href="https://www.gatsbyjs.org">
+    <img alt="Gatsby" src="https://www.gatsbyjs.org/monogram.svg" width="60" />
+  </a>
+</p>
+
+<h1 align="center">
+  My Website
+</h1>
+
+Written in [React](https://reactjs.org/), using [Gatsby](https://gatsbyjs.org), the [minimal blog theme](https://minimal-blog.lekoarts.de/) by [LekoArts](https://www.lekoarts.de/en), and hosted with [Netlify](https://netlify.com).
---
+++ gatsby-config.js
@@ -0,0 +1,75 @@
+require(`dotenv`).config({
+  path: `.env`,
+})
+
+module.exports = {
+  siteMetadata: {
+    siteTitle: `Serdar KÖKTAS`,
+    siteHeadline: `Serdar KÖKTAŞ | Personal Website, blog.`,
+    siteTitleAlt: `Serdar KÖKTAŞ - Student | Adana,TR`,
+    siteDescription: `Serdar KÖKTAŞ | Personal Website, blog.`,
+    siteImage: `/banner.jpg`,
+    siteUrl: `https://serdarkoktas.com/`,
+    author: `@serdarkkts`,
+    navigation: [
+      {
+        title: `About`,
+        slug: `/about`,
+      },
+      {
+        title: `Blog`,
+        slug: `/blog`,
+      },
+    ],
+    externalLinks: [
+      {
+        name: `LinkedIn`,
+        url: `https://www.linkedin.com/in/serdarkkts`
+      },
+      {
+        name: `Github`,
+        url: `https://www.github.com/serdarkkts/`
+      }
+    ],
+  },
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-minimal-blog`,
+      options: {},
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+      },
+    },
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Serdar KÖKTAŞ`,
+        short_name: `Serdar`,
+        description: `Personal Website,blog`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#6B46C1`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify`,
+    // `gatsby-plugin-webpack-bundle-analyser-v2`,
+  ],
+}
